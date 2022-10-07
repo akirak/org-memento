d0ccd93185023e705add153b53820733f8141860
Akira Komamura <akira.komamura@gmail.com>
2022-10-07 15:49:13 +0900

    test: Add linting

---
+++ .nomake/flake.nix
@@ -0,0 +1,13 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    org-memento = {
+      flake = false;
+      owner = "~akirak";
+      repo = "org-memento";
+      type = "sourcehut";
+    };
+  };
+  outputs = { ... }: { };
+}
