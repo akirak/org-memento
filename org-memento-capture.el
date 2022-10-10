18a0c7bfa276936d9115b52e62e6e870194a9a1b
Akira Komamura <akira.komamura@gmail.com>
2022-10-10 16:37:35 +0900

    Remove the infix for setting the time range
    See the suffix commands.

--- org-memento-capture.el
+++ org-memento-capture.el
@@ -60,62 +60,6 @@ The value should be an internal time of the midnight on the date.")
   :class 'org-memento-capture--date-class
   :variable 'org-memento-capture--date)
 
-;;;;; Time range
-
-(defvar org-memento-capture--time nil
-  "Time range in (START END) format.
-
-START and END are internal time representations.")
-
-(defclass org-memento-capture--time-class (org-memento-capture--variable-class)
-  ())
-
-(cl-defmethod transient-infix-read ((obj org-memento-capture--time-class))
-  (let* ((value (oref obj value))
-         (midnight (or org-memento-capture--date
-                       (org-memento-capture--default-date))))
-    (cl-flet*
-        ((to-time (time)
-           (time-add midnight (* 60 time)))
-         (read-time (initial start)
-           (pcase (org-memento-read-time-of-day
-                   :start-time start
-                   :decoded-date (decode-time org-memento-capture--date)
-                   :initial-value (when initial
-                                    (/ (- (float-time initial)
-                                          (float-time midnight))
-                                       60)))
-             (`nil)
-             (`(,newstart ,newend)
-              (list (to-time newstart)
-                    (to-time newend)))
-             (time
-              (to-time time)))))
-      (let ((t1 (read-time (car value) nil)))
-        (pcase t1
-          (`(,_ ,_)
-           t1)
-          (start
-           (list start (read-time (nth 1 value) start))))))))
-
-(cl-defmethod transient-format-value ((obj org-memento-capture--time-class))
-  (pcase (oref obj value)
-    (`(,start ,end)
-     (concat
-      (propertize "(" 'face 'transient-inactive-value)
-      (propertize (concat (format-time-string "%R" start)
-                          "-"
-                          (format-time-string "%R" end))
-                  'face 'transient-value)
-      (propertize ")" 'face 'transient-inactive-value)))
-    (_
-     "")))
-
-(transient-define-infix org-memento-capture--time-infix ()
-  :description "Time range"
-  :class 'org-memento-capture--time-class
-  :variable 'org-memento-capture--time)
-
 ;;;; Suffix commands
 
 (defun org-memento-capture-log ()
