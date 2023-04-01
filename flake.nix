bb9f7b839f7df7388125556cd211c3361f29a7cf
Akira Komamura <akira.komamura@gmail.com>
2023-04-01 21:19:51 +0900

    Test exporting

--- flake.nix
+++ flake.nix
@@ -57,6 +57,23 @@
             emacs -batch -L . -l buttercup -f buttercup-run-discover
           '';
         };
+
+        test-export = {
+          compile = false;
+          text = ''
+            emacs -batch -L . --eval "(setq org-memento-file \"test/memento3.org\")" \
+              -l org-memento-export \
+              --eval "(setq make-backup-files nil)" \
+              --eval "(org-memento-export-to-json \"2021-12-31\" \"2021-12-31\" \"test/export.json\")"
+
+            # You need to have a node executable in the testing environment.
+            # Due to a limitation with this framework, it cannot be provided
+            # from Nix, but it runs on GitHub Actions.
+
+            cd node
+            node index.js validate -f ../test/export.json
+          '';
+        };
       };
     };
 }
